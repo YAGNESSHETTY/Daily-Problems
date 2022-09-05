030f380bf3c83324ec1c4f8458aba27b308bbea5
YAGNESSHETTY <80250245+YAGNESSHETTY@users.noreply.github.com>
2022-09-06 02:46:56 +0530

    Update q1gfgdaily.cpp

--- Day2-06.09.2022/q1gfgdaily.cpp
+++ Day2-06.09.2022/q1gfgdaily.cpp
@@ -1,1 +1,13 @@
-
+class Solution{
+public:
+    long long findMinSum(vector<int> &A,vector<int> &B,int N){
+        sort(A.begin() , A.end()) ;
+        sort(B.begin() , B.end()) ;
+        
+        long long ans = 0 ;
+        for(int i = 0; i  < N ; i++){
+            ans+=(abs(A[i] - B[i])) ;
+        }
+        return ans ;
+    }
+};
